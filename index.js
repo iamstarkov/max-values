bd480aa53648894924629356c6345d9c94b3536f
Vladimir Starkov <iamstarkov@gmail.com>
2015-08-20 19:50:02 +0200

    curry all the things

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-import { maxBy, path, split } from 'ramda';
+import { maxBy, path, split, apply, compose } from 'ramda';
 import camelcase from 'camelcase';
 
-const splitByDot = split('.');
+const maxByProp = compose(apply, maxBy, path, split('.'));
 
 export default function maxValues(arr, props) {
   if (!arr || !props) return;
-  props.forEach(prop => {
-    const maxByProp = maxBy(path(splitByDot(prop)));
-    const camelCaseMaxProp = camelcase('max', prop);
-    maxByProp.apply(null, arr)[camelCaseMaxProp] = true;
-  });
+  props.forEach(prop => { maxByProp(prop)(arr)[camelcase('max', prop)] = true; });
   return arr;
 };
