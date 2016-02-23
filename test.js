6dc651f8598ec30c69b3c434ccd3fccf7f341e15
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-23 18:26:11 +0100

    add failing test

--- test.js
+++ test.js
@@ -3,13 +3,15 @@ import maxValues from './index';
 
 const input = [{ name: 'Bob',   age: 25, skills: { crypto: 1  }},
                { name: 'Some',  age: 14, skills: { crypto: 5 }},
-               { name: 'Alice', age: 7,  skills: { crypto: 10 }}]
+               { name: 'Alice', age: 7,  skills: { crypto: 10 }},
+               { name: 'Gato',  age: 8 }]
 
 it('should maxValues', () =>
   deepEqual(maxValues(input, ['age', 'skills.crypto']),
     [{ name: 'Bob',   age: 25, skills: { crypto: 1  }, maxAge: true },
      { name: 'Some',  age: 14, skills: { crypto: 5 }},
-     { name: 'Alice', age: 7,  skills: { crypto: 10 }, maxSkillsCrypto: true }]));
+     { name: 'Alice', age: 7,  skills: { crypto: 10 }, maxSkillsCrypto: true },
+     { name: 'Gato',  age: 8 }]));
 
 it('should maxValues invalid input', () =>
   equal(maxValues(), undefined));
