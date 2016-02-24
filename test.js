51a608dcb78d665f459f9c904982bd76edb5302f
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-24 02:42:36 +0100

    simplify tests

--- test.js
+++ test.js
@@ -6,12 +6,13 @@ const input = [{ name: 'Bob',   age: 25, skills: { crypto: 1  }},
                { name: 'Alice', age: 7,  skills: { crypto: 10 }},
                { name: 'Gato',  age: 8 }]
 
+const output = [{ name: 'Bob',   age: 25, skills: { crypto: 1  }, maxAge: true },
+                { name: 'Some',  age: 14, skills: { crypto: 5 }},
+                { name: 'Alice', age: 7,  skills: { crypto: 10 }, maxSkillsCrypto: true },
+                { name: 'Gato',  age: 8 }];
+
 it('should maxValues', () =>
-  deepEqual(maxValues(input, ['age', 'skills.crypto']),
-    [{ name: 'Bob',   age: 25, skills: { crypto: 1  }, maxAge: true },
-     { name: 'Some',  age: 14, skills: { crypto: 5 }},
-     { name: 'Alice', age: 7,  skills: { crypto: 10 }, maxSkillsCrypto: true },
-     { name: 'Gato',  age: 8 }]));
+  deepEqual(maxValues(input, ['age', 'skills.crypto']), output));
 
 it('should maxValues invalid input', () =>
   equal(maxValues(), undefined));
