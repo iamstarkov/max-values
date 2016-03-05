adf4fe6343f59d30046227dfbf242315c1b971d2
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-05 23:31:05 +0100

    refactoring tests

--- test.js
+++ test.js
@@ -1,18 +1,10 @@
 import { equal, deepEqual } from 'assert';
 import maxValues from './index';
 
-const input = [{ name: 'Bob',   age: 25, skills: { crypto: 1  }},
-               { name: 'Some',  age: 14, skills: { crypto: 5 }},
-               { name: 'Alice', age: 7,  skills: { crypto: 10 }},
-               { name: 'Gato',  age: 8 }]
+import simple from './simple.json';
 
-const output = [{ name: 'Bob',   age: 25, skills: { crypto: 1  }, maxAge: true },
-                { name: 'Some',  age: 14, skills: { crypto: 5 }},
-                { name: 'Alice', age: 7,  skills: { crypto: 10 }, maxSkillsCrypto: true },
-                { name: 'Gato',  age: 8 }];
-
-it('should maxValues', () =>
-  deepEqual(maxValues(input, ['age', 'skills.crypto']), output));
+it('should simple maxValues', () =>
+  deepEqual(maxValues(simple.input, simple.props), simple.expected));
 
 it('should maxValues invalid input', () =>
   equal(maxValues(), undefined));
