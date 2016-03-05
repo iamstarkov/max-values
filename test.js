60434ab804f1d2d3d2a5d4f339497cff79f4fd08
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-05 23:33:05 +0100

    add failing tests for complicated case

--- test.js
+++ test.js
@@ -2,9 +2,13 @@ import { equal, deepEqual } from 'assert';
 import maxValues from './index';
 
 import simple from './simple.json';
+import complicated from './complicated.json';
 
 it('should simple maxValues', () =>
   deepEqual(maxValues(simple.input, simple.props), simple.expected));
+
+it('should complicated maxValues', () =>
+  deepEqual(maxValues(complicated.input, complicated.props), complicated.expected));
 
 it('should maxValues invalid input', () =>
   equal(maxValues(), undefined));
