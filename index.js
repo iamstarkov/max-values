88b9ea77b0c7858dea021918f8ade8b96a9ba124
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-06 02:36:20 +0100

    fix complicated stuff

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import camelcase from 'camelcase';
 const sortBy = R.pipe(R.split('.'), R.path, R.sortBy);
 const isNotNil = R.pipe(R.isNil, R.not);
 const sortedAndFiltered = (prop, arr) => R.pipe(
-  sortBy(prop),
   R.filter(R.pathSatisfies(isNotNil, R.split('.', prop))),
+  sortBy(prop),
   R.last)(arr);
 
 export default function maxValues(arr, props) {
